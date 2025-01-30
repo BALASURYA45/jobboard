90a390a34dfdc8608ab1f2bb110b3213806c5564
BALASURYA45 <balasurya.d2023cse@sece.ac.in>
2025-01-30 11:53:59 +0530

    changes

--- Frontend/src/pages/ResumeUploaderPage.jsx
+++ Frontend/src/pages/ResumeUploaderPage.jsx
@@ -4,7 +4,7 @@ import ResumeUploader from '../components/ResumeUploader';
 const ResumeUploaderPage = () => {
   return (
     <div>
-      <h1>Resume Uploader Page</h1>
+      <h1>Resume Uploader</h1>
       <ResumeUploader />
     </div>
   );
